4e7650a470ec95f4fa3bfb097c77591962391f24
alve06001 <alve06001@E-PW0234LA>
2023-09-13 10:50:50 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken stad kommer du ifrån? ");
+            String stad = Console.ReadLine();
+            Console.WriteLine("Vad är ditt dröm resemål?");
+            String resemål = Console.ReadLine();
+            Console.ReadKey();
+            Console.WriteLine("Hej " + stad + "'s bo. Jag hoppas att du kan åka till " + resemål + " någon gång.");
+        }
+    }
+}
